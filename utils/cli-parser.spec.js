525e149ed6345f3b0c15ee8b67877a8b9132f3c6
Emre Kara <emre1702@live.de>
2022-04-29 09:43:41 +0200

    Add insecure to cli-parser.spec

--- utils/cli-parser.spec.js
+++ utils/cli-parser.spec.js
@@ -2,8 +2,8 @@ const cliParser = require('./cli-parser.js');
 
 describe('cli-parser', () => {
   it('should return valid runner args', () => {
-    process.argv = ['path', '--collection=bar', '--runCount=20', '--foo=bar'];
+    process.argv = ['path', '--collection=bar', '--runCount=20', '--foo=bar', '--insecure'];
     const args = cliParser.getCliArguments();
-    expect(args).toStrictEqual({ collection: 'bar', runCount: 20 });
+    expect(args).toStrictEqual({ collection: 'bar', runCount: 20, insecure: true });
   });
 });
