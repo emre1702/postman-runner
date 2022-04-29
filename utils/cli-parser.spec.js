2dab6b9e8ec82bb549f614b94592b5d48914e6aa
Emre Kara <emre1702@live.de>
2022-04-29 09:48:08 +0200

    Update cli-parser.spec.js

--- utils/cli-parser.spec.js
+++ utils/cli-parser.spec.js
@@ -2,7 +2,7 @@ const cliParser = require('./cli-parser.js');
 
 describe('cli-parser', () => {
   it('should return valid runner args', () => {
-    process.argv = ['path', '--collection=bar', '--runCount=20', '--foo=bar', '--insecure'];
+    process.argv = ['path', '--collection=bar', '--runCount=20', '--foo=bar', '--insecure=true'];
     const args = cliParser.getCliArguments();
     expect(args).toStrictEqual({ collection: 'bar', runCount: 20, insecure: true });
   });
